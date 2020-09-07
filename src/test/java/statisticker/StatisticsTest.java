83af0ad3ebb5589c4bee86ad450f14e181d5cd56
nihal <nihalpereira1998@gmail.com>
2020-09-07 18:38:27 +0530

    added tests

--- src/test/java/statisticker/StatisticsTest.java
+++ src/test/java/statisticker/StatisticsTest.java
@@ -1,13 +1,15 @@
 package statisticker;
 
-import static org.junit.Assert.*;
-import jdk.nashorn.internal.AssertsEnabled;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
+//import jdk.nashorn.internal.AssertsEnabled;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class StatisticsTest 
 {
@@ -15,7 +17,7 @@ public class StatisticsTest
     public void reportsAverageMinMaxx()
     {
         Float[] numbers = {1.5f, 8.9f, 3.2f, 4.5f};
-        List<___> numberList = Arrays.asList(numbers);
+        List<Float> numberList = Arrays.asList(numbers);
 
         Statistics.Stats s = Statistics.getStatistics(numberList);
 
@@ -27,13 +29,12 @@ public class StatisticsTest
     @Test
     public void reportsNaNForEmptyInput()
     {
-        List<___> emptyList = new ArrayList<___>();
+        List<Float> emptyList = new ArrayList<>();
 
         Statistics.Stats s = Statistics.getStatistics(emptyList);
 
-        //All fields of computedStats (average, max, min) must be
-        //Float.NaN (not-a-number), as described in
-        //https://www.geeksforgeeks.org/nan-not-number-java/
-        //Design the asserts here and implement accordingly.
+        Assert.assertTrue(s.average.isNaN());
+        Assert.assertTrue(s.min.isNaN());
+        Assert.assertTrue(s.max.isNaN());
     }
 }
